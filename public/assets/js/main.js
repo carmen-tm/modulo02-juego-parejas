00b8adaf7abad0e38e1e12e7a717930b64b4a21c
carmen-tm <carmentorremolina@gmail.com>
2019-05-05 01:33:52 +0200

    Add instructions to identify a pair and show EUREKA on html

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -5,22 +5,26 @@
 const inputsRadioEls = document.querySelectorAll('.number-cards__button');
 const btnStartEl = document.querySelector('.btn-start');
 const listResultsEl = document.querySelector('.app-results');
+const mainContainerEl = document.querySelector('.main-container');
 
 let NUMBER = '';
 let cardsFromAPI = [];
+
+let pairCards = [];
 
 //Functions
 function drawCards(array) {
   listResultsEl.innerHTML = '';
   //Paint as many cards as the user choice
   for (const element of array) {
-    const cardImageUrl = element.image;
-    console.log(cardImageUrl);
+    const { image, pair } = element;
 
     const itemEl = document.createElement('li');
     const divFaceEl = document.createElement('div');
-    divFaceEl.style.backgroundImage = `url(${cardImageUrl})`;
+    divFaceEl.style.backgroundImage = `url(${image})`;
+    divFaceEl.setAttribute('data-pair', pair);
     divFaceEl.classList.add('card', 'card--face', 'card--hidden');
+
     const divBackEl = document.createElement('div');
     divBackEl.classList.add('card', 'card--back');
 
@@ -43,6 +47,49 @@ function handlerCardsClick(event) {
   if (selectedCardBackEl) {
     selectedCardBackEl.classList.toggle('card--hidden');
   }
+
+  //Instructions just to be applied when one card is already up. Condition??
+  const cardsDown = [];
+  const cardsUp = [];
+  const allCardsFaceEl = document.querySelectorAll('.card--face');
+  for (const card of allCardsFaceEl) {
+    if (card.classList.contains('card--hidden')) {
+      cardsDown.push(card);
+    } else {
+      cardsUp.push(card);
+    }
+  }
+  const cardsUpNow = NUMBER - cardsDown.length;
+
+  const selectedCardPairId = parseInt(
+    selectedCardFaceEl.getAttribute('data-pair')
+  );
+  console.log(cardsUpNow);
+  if (cardsUpNow === 1) {
+    pairCards = [];
+    pairCards[0] = selectedCardPairId;
+    console.log(selectedCardPairId);
+  } else if (cardsUpNow === 2) {
+    pairCards[1] = selectedCardPairId;
+    console.log('solo hay una carta boca arriba');
+    if (pairCards[0] === pairCards[1]) {
+      //Print message on HTML
+      const messageEl = document.createElement('p');
+      const messageText = document.createTextNode('EUREKA!!!');
+      messageEl.classList.add('eureka');
+      messageEl.appendChild(messageText);
+      mainContainerEl.appendChild(messageEl);
+
+      // for (const card of cardsUp) {
+      //   setInterval(card.classList.add('card--hidden'), 3000);
+      // }
+      console.dir(cardsDown);
+    } else {
+      console.log('no coinciden');
+    }
+  } else {
+    console.log('más de una carta boca arriba');
+  }
 }
 
 function requestApi(URL) {
@@ -57,11 +104,10 @@ function requestApi(URL) {
     })
     .then(function(data) {
       const cardsEls = document.querySelectorAll('.card');
-      console.log(cardsEls);
       for (const element of cardsEls) {
         element.addEventListener('click', handlerCardsClick);
       }
-      // console.log(data, cardsEls);
+      console.log(cardsFromAPI);
     });
 }
 
