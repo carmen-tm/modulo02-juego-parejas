b47252cf73c50674c79dd412a0344fed0d65c3fc
carmen-tm <carmentorremolina@gmail.com>
2019-05-05 00:33:52 +0200

    Finish point4-interaction

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -14,16 +14,34 @@ function drawCards(array) {
   listResultsEl.innerHTML = '';
   //Paint as many cards as the user choice
   for (const element of array) {
-    const itemEl = document.createElement('li');
+    const cardImageUrl = element.image;
+    console.log(cardImageUrl);
 
+    const itemEl = document.createElement('li');
     const divFaceEl = document.createElement('div');
-    divFaceEl.classList.add('card', 'card--face');
+    divFaceEl.style.backgroundImage = `url(${cardImageUrl})`;
+    divFaceEl.classList.add('card', 'card--face', 'card--hidden');
     const divBackEl = document.createElement('div');
     divBackEl.classList.add('card', 'card--back');
 
     itemEl.appendChild(divFaceEl);
     itemEl.appendChild(divBackEl);
     listResultsEl.appendChild(itemEl);
+  }
+}
+
+function handlerCardsClick(event) {
+  const selectedCardEl = event.currentTarget;
+  selectedCardEl.classList.toggle('card--hidden');
+
+  const selectedCardFaceEl = selectedCardEl.previousSibling;
+  const selectedCardBackEl = selectedCardEl.nextSibling;
+
+  if (selectedCardFaceEl) {
+    selectedCardFaceEl.classList.toggle('card--hidden');
+  }
+  if (selectedCardBackEl) {
+    selectedCardBackEl.classList.toggle('card--hidden');
   }
 }
 
@@ -35,6 +53,15 @@ function requestApi(URL) {
     .then(function(responsedParsed) {
       cardsFromAPI = responsedParsed;
       drawCards(cardsFromAPI);
+      return cardsFromAPI;
+    })
+    .then(function(data) {
+      const cardsEls = document.querySelectorAll('.card');
+      console.log(cardsEls);
+      for (const element of cardsEls) {
+        element.addEventListener('click', handlerCardsClick);
+      }
+      // console.log(data, cardsEls);
     });
 }
 
